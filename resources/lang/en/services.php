74dbc09c652a5749a05bb29a697b9a5a2578c456
Michael Joyner <joyner.michael@gmail.com>
2019-02-22 11:03:58 +0800

    services page

--- resources/lang/en/services.php
+++ resources/lang/en/services.php
@@ -1,5 +1,34 @@
 <?php
 
 return [
-    'message' => 'This is the services page'
+    'intro' => [
+        'heading' => 'A wide range of accounting services for all your needs.',
+        'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae dicta eligendi iusto magnam maxime nihil odit quos sunt. Incidunt nulla, vitae? Consequuntur dolores ducimus eum illum magnam perspiciatis praesentium, tenetur!'
+    ],
+
+    'audits' => [
+        'heading' => 'Audits',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus deleniti ducimus esse excepturi expedita fugiat ipsa iste laboriosam neque perferendis qui, quis quos, sapiente sint veritatis. Beatae cum ducimus labore.',
+        'link' => 'More about Audits &#155;'
+    ],
+
+    'bookkeeping' => [
+        'heading' => 'Bookkeeping',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad adipisci alias aut dolores laborum nobis placeat possimus quibusdam, vel. Adipisci cum eveniet, itaque iusto libero nihil obcaecati possimus quo.',
+        'link' => 'More about Bookkeeping &#155;'
+
+
+   ],
+
+    'tax' => [
+        'heading' => 'Tax',
+        'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad animi at consequatur consequuntur corporis culpa dolorem doloremque ex excepturi, facere fugiat, incidunt molestiae molestias porro ratione reiciendis sit totam voluptatibus.',
+        'link' => 'More about Tax &#155;'
+    ],
+    
+    'business_registration' => [
+        'heading' => 'Business Registration',
+        'description' => '    Lorem ipsum dolor sit amet, consectetur adipisicing elit. At doloribus esse fugiat illum iure modi voluptatum. A animi asperiores, cupiditate eaque fugiat ipsum iste, pariatur porro possimus sapiente vero voluptate.',
+        'link' => 'More about Business Reg. &#155;'
+    ]
 ];
--- resources/views/front/services/index.blade.php
+++ resources/views/front/services/index.blade.php
@@ -1,5 +1,45 @@
-@extends('front.base')
+@extends('front.base', ['bodyClasses' => 'pt-12'])
 
 @section('content')
-    <h1>{{ trans('services.message') }}</h1>
+    <section class="reg-section-space">
+        <div class="max-w-md mx-auto">
+            <h1 class="h1 text-orange">{{ trans('services.intro.heading') }}</h1>
+            <p class="mt-12">{{ trans('services.intro.content') }}</p>
+        </div>
+    </section>
+
+    <section class="max-w-lg mx-auto">
+        <div class="mb-20 p-8 bg-pale-baby-blue">
+            <div class="max-w-md mx-auto">
+                <h2 class="h2 text-orange">{{ trans('services.audits.heading') }}</h2>
+                <p class="mt-8">{{ trans('services.audits.description') }}</p>
+                <a href="" class="mt-12 text-link text-navy block text-center">{!! trans('services.audits.link') !!}</a>
+            </div>
+        </div>
+
+        <div class="mb-20 p-8 bg-pale-baby-blue">
+            <div class="max-w-md mx-auto">
+                <h2 class="h2 text-orange">{{ trans('services.bookkeeping.heading') }}</h2>
+                <p class="mt-8">{{ trans('services.bookkeeping.description') }}</p>
+                <a href="" class="mt-12 text-link text-navy block text-center">{!! trans('services.bookkeeping.link') !!}</a>
+            </div>
+        </div>
+
+        <div class="mb-20 p-8 bg-pale-baby-blue">
+            <div class="max-w-md mx-auto">
+                <h2 class="h2 text-orange">{{ trans('services.tax.heading') }}</h2>
+                <p class="mt-8">{{ trans('services.tax.description') }}</p>
+                <a href="" class="mt-12 text-link text-navy block text-center">{!! trans('services.tax.link') !!}</a>
+            </div>
+        </div>
+
+        <div class="mb-20 p-8 bg-pale-baby-blue">
+            <div class="max-w-md mx-auto">
+                <h2 class="h2 text-orange">{{ trans('services.business_registration.heading') }}</h2>
+                <p class="mt-8">{{ trans('services.business_registration.description') }}</p>
+                <a href="" class="mt-12 text-link text-navy block text-center">{!! trans('services.business_registration.link') !!}</a>
+            </div>
+        </div>
+    </section>
+
 @endsection
