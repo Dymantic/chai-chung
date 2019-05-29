bc3930fad5894c500c67e4ddba99d84b2dc8801c
Michael Joyner <joyner.michael@gmail.com>
2019-05-29 09:22:14 +0800

    correct links in buz ass subnav

--- resources/views/front/services/business-assistance/subnav-item.blade.php
+++ resources/views/front/services/business-assistance/subnav-item.blade.php
@@ -1,5 +1,5 @@
 @if($isLink)
-<a href="{{ localUrl("/business-formation") }}" class="no-underline">
+<a href="{{ $address }}" class="no-underline">
 @endif
     <div class="relative flex md:justify-start md:justify-center items-center text-center my-4 md:my-0 p-4 md:w-32 md:h-32 bg-grey-lighter @if($isLink) shadow-lg hover:bg-orange-light @endif  mx-4 rounded no-underline text-navy font-bold">
         <span class="md:absolute md:pin-t md:pin-l mr-4 text-orange md:pl-2">{{ $number }}.</span>
