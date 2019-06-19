0bf281ef3a281478f3ab66398654560eb05fa3df
Michael Joyner <joyner.michael@gmail.com>
2019-06-19 09:42:03 +0800

    use better map address

--- resources/views/front/contact/page.blade.php
+++ resources/views/front/contact/page.blade.php
@@ -15,5 +15,5 @@
         </div>
 
     </section>
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3640.6353787917665!2d120.64645468447416!3d24.149439941871073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34693d965c93dccd%3A0x9e365a9054bd0389!2s%E6%A8%93%E4%B9%8B2%2C+No.+61%2C+Section+2%2C+Gongyi+Road%2C+Nantun+District%2C+Taichung+City%2C+408!5e0!3m2!1sen!2stw!4v1551144006614" width="600" height="450" class="w-full" frameborder="0" style="border:0" allowfullscreen></iframe>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3640.6007939641504!2d120.64828931544035!3d24.150653879283702!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x34693d965c93dccd%3A0xf977bbe82b73f74e!2z5ZiJ55y-5pyD6KiI5bir5LqL5YuZ5omA!5e0!3m2!1sen!2stw!4v1560908414009!5m2!1sen!2stw" class="w-full" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 @endsection
