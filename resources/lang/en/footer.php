8f3c56b3d9a8c8a304381d01410739abb34a3c37
Michael Joyner <joyner.michael@gmail.com>
2019-07-31 08:34:54 +0800

    small fixes of copy

--- resources/lang/en/footer.php
+++ resources/lang/en/footer.php
@@ -12,7 +12,7 @@ return [
     'contact_info' => [
         'phone' => 'T: 886-4-23208012',
         'fax' => 'F: 886-4-23208022',
-        'email' => 'service@chia-chung.com',
+        'email' => 'service@hj-cpas.com',
         'address_line_one' => 'No. 61, Section 2, Gongyi Rd,',
         'address_line_two' => 'Nantun Dist., Taichung City, 408',
     ],
--- resources/views/admin/partials/fakenavbar.blade.php
+++ resources/views/admin/partials/fakenavbar.blade.php
@@ -1,5 +1,5 @@
 <div class="h-12 bg-navy flex justify-between items-center px-4">
-    <div class="font-black text-orange">Tinos</div>
+    <div class="font-black text-orange">嘉眾會計師事務所</div>
     <div class="flex items-center justify-end">
 
     </div>
