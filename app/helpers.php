b5bad20b87d456c3e94e1cd8beaa6d2ed80e69bf
Michael Joyner <joyner.michael@gmail.com>
2019-10-27 09:59:41 +0800

    upgrade to six

--- app/helpers.php
+++ app/helpers.php
@@ -1,16 +1,18 @@
 <?php
 
+use Illuminate\Support\Str;
+
 function localUrl($url) {
     return sprintf("/%s/%s", app()->getLocale(), ltrim($url, '/'));
 }
 
 function transUrl($url) {
-    $url = str_start($url, '/');
+    $url = Str::start($url, '/');
 
     if($url === '/en' || $url === '/zh') {
         $url = $url . '/';
     }
-    if(starts_with($url, ['/en/', '/zh/'])) {
+    if(Str::startsWith($url, ['/en/', '/zh/'])) {
         $url = mb_substr($url, 4);
     }
 
